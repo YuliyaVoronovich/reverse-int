7cdebdf07c2661bcc8f82258d1e60c10967a6698
YuliyaVoronovich <Y-1306@mail.ru>
2023-07-29 12:39:49 +0300

    add: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+
+  let result = 0;
+  n = Math.abs(n);
+
+    while (n) {
+        
+        let resultDivision = n % 10;
+
+        n = Math.floor(n / 10);
+        result = result * 10 + resultDivision;
+    }
+
+    return result;
+
 }
